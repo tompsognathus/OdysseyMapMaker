e05f8e970e4832f0d68bce9e8e868633e6f4cc1a
tompsognathus <tompsognathus@gmail.com>
2023-09-28 17:52:38 +0100

    added toolbox panel widget blueprint

---
+++ Source/OdysseyMapMaker/Toolbox.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Toolbox.h"
+
+// Sets default values
+AToolbox::AToolbox()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AToolbox::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AToolbox::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
